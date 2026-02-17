81cbb1109784618034fd1397edeba3ab09e999cf
mateuscaldasjr-create <mateuscaldasjr@gmail.com>
2026-02-17 12:10:37 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
+3. Ajuste o src/App.tsx
+O TypeScript está reclamando que você está chamando componentes (como Sidebar e Header) sem passar as informações obrigatórias para eles (como o profile ou role).
 
+Substitua o conteúdo do seu src/App.tsx por esta versão "ajustada" para passar no build:
